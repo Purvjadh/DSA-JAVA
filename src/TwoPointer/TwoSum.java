6b2b37484fdd4679dbdc2c3abb2a8a7c150b9c93
PurvaJadhav <purvajadhav1604@gmail.com>
2026-09-02 23:54:02 +0530

    add TwoSum Solution

---
+++ src/TwoPointer/TwoSum.java
@@ -0,0 +1,39 @@
+package TwoPointer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoSum {
+
+    public static boolean twoSum(int [] nums,int target){
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<right){
+            int sum = nums[left] + nums[right];
+            if(sum == target){
+                return true;
+            }else if(sum < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int [] nums = {0,-1,2,-3,1};
+        int target= -2;
+
+        if(twoSum(nums,target)){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+
+    }
+}
